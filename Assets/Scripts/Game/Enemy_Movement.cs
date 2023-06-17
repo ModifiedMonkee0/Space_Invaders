260dc71ab872c8d60206a34443c14acf57ea9702
Baran Avcı <baranavci35@gmail.com>
2023-06-17 18:16:11 +0300

    Last

--- Assets/Scripts/Game/Enemy_Movement.cs
+++ Assets/Scripts/Game/Enemy_Movement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy_Movement : MonoBehaviour
 {
     public float moveSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,4 +28,6 @@ public class Enemy_Movement : MonoBehaviour
             moveSpeed *= -1;
         }
     }
+
+   
 }
